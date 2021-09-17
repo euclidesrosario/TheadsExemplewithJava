dfef30769704828e0f1b02472b757918446cc930
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-09-17 12:54:36 +0200

    n

--- src/TheadsExameple.java
+++ src/TheadsExameple.java
@@ -1,30 +1,39 @@
+
 // We import java.lang so we can have access the interfaces Theads and runnable 
-import java.lang.*; 
+import java.lang.*;
+
 public class TheadsExameple {
-	
+
+	/**
+	 * Inside the method main I'm instanced and started two threads, these threads
+	 * run the methods firstthred and secondthead at the same time ie asyncrhonusly
+	 */
 	public static void main() {
-		
+
+		new Thread(firstThread).start();
+		new Thread(SecondThread).start();
+
 	}
-	
-	private static Runnable firstThead = new Runnable() {
-		
+
+	private static Runnable firstThread = new Runnable() {
+
 		public void run() {
-			for (int i = 10; i<10; i++) {
-				
-				System.out.println("This print come from  the firstThead" + 1 );
+			for (int i = 10; i < 10; i++) {
+
+				System.out.println("This print come from  the firstThead" + 1);
 			}
-			
+
 		}
 	};
-	
-	public static Runnable SecondThead = new Runnable() {
-		
+
+	public static Runnable SecondThread = new Runnable() {
+
 		public void run() {
-for (int i = 10; i<10; i++) {
-				
-				System.out.println("This print come from  the SecondtThead" + 1 );
+			for (int i = 10; i < 10; i++) {
+
+				System.out.println("This print come from  the SecondtThead" + 1);
 			}
 		}
 	};
-	
+
 }
