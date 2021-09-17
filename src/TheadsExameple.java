7c2d8d8f1f980b07cce9c89349e216d037e5431b
euclidesrosario <euclidesdeazevedo@protonmail.com>
2021-09-17 12:08:59 +0200

    I'm creating the first thead
    This method print the  this message "this print come from the first thead" and include a value of position of loop

--- src/TheadsExameple.java
+++ src/TheadsExameple.java
@@ -1,4 +1,9 @@
-
+// We import java.lang so we can have access the interfaces Theads and runnable 
+import java.lang.*; 
 public class TheadsExameple {
-
+	
+	public static void main() {
+		
+	}
+	
 }
